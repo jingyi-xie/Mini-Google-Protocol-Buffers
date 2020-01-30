0322938654ebbf9f94169457ee4925f4f1c364b6
Jingyi Xie <jingyi.xie@duke.edu>
2020-01-30 11:57:17 -0500

    finished task 2

--- src/main/java/edu/duke/ece651/classbuilder/ClassBuilder.java
+++ src/main/java/edu/duke/ece651/classbuilder/ClassBuilder.java
@@ -1,24 +1,52 @@
 package edu.duke.ece651.classbuilder;
 
+//import java.util.Iterator; 
+import java.util.Map;
+import java.io.InputStream;
 import org.json.*;
 
 public class ClassBuilder {
-  private String name;
+  //private String className;
+  //private Map<String, String> fields;
+  public String className;
+  public String name;
+  public String type;
+  //public Map<String, String> fields;
+
+  private void construct_helper(JSONObject jo) {
+    JSONArray classes = jo.getJSONArray("classes");
+    JSONObject oneclass = classes.getJSONObject(0);
+    //Iterator it = classes.iterator();
+    
+    className = oneclass.getString("name");
+    JSONArray ja = oneclass.getJSONArray("fields");
+    JSONObject onefield = ja.getJSONObject(0);
+    name = onefield.getString("name");
+    type = onefield.getString("type");
+    //fields.put(onefield.getString("name"), onefield.getString("type"));
+  }
   public ClassBuilder(String str) {
-    JSONObject j = new JSONObject(str); 
-    name = j.getString("name");
+    JSONObject jo = new JSONObject(str);
+    construct_helper(jo); 
   }
-
-  public String getClassNames() {
-    return name;
+  public ClassBuilder(InputStream input) {
+    JSONObject jo = new JSONObject(input); 
+    construct_helper(jo);
   }
-
-  //public ClassBuilder(InputStream input) {
-  //}
-  
+}
+class Main {
   public static void main(String[] args) {
-    ClassBuilder cb = new ClassBuilder("{\"name\" : \"Test\"}"); 
-    String name = cb.getClassNames();
-    System.out.println(name);
+    String str = "{\"classes\" : [{\"name\" : \"Test\", \"fields\" : [{\"name\" : \"x\", \"type\" : \"int\"}]}]}";
+    ClassBuilder cb = new ClassBuilder(str);
+    System.out.println(cb.className);
+    System.out.println(cb.name);
+    System.out.println(cb.type);
+    /*
+    for (Map.Entry<String, String> entry : cb.fields.entrySet()) {
+      String k = entry.getKey();
+      String v = entry.getValue();
+      System.out.println("Key: " + k + ", Value: " + v);
+    }
+    */
   }
 }
--- src/test/java/edu/duke/ece651/classbuilder/CBTest.java
+++ src/test/java/edu/duke/ece651/classbuilder/CBTest.java
@@ -3,12 +3,19 @@ package edu.duke.ece651.classbuilder;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import java.util.Map;
 
 public class CBTest {
   @Test
   public void test_classbuilder() {
-    ClassBuilder cb = new ClassBuilder("{\"name\" : \"Test\"}");
-    assertEquals(cb.getClassNames(), "Test");
+    String str = "{\"classes\" : [{\"name\" : \"Test\", \"fields\" : [{\"name\" : \"x\", \"type\" : \"int\"}]}]}";
+    ClassBuilder cb = new ClassBuilder(str);
+    assertEquals(cb.className, "Test");
+    assertEquals(cb.name, "x");
+    assertEquals(cb.type, "int");
+    
+    
+    
   }
   
 
