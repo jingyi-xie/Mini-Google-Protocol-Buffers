4833aabfb4ed900599d1ea618dcab10f6ad77a1d
Jingyi Xie <jingyi.xie@duke.edu>
2020-01-31 00:41:45 -0500

    complete task 3

--- src/main/java/edu/duke/ece651/classbuilder/ClassBuilder.java
+++ src/main/java/edu/duke/ece651/classbuilder/ClassBuilder.java
@@ -1,39 +1,61 @@
 package edu.duke.ece651.classbuilder;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 //import java.util.Iterator; 
-import java.util.Map;
+//import java.util.Map;
 import java.io.InputStream;
 import org.json.*;
+import java.io.PrintWriter;
 
 public class ClassBuilder {
-  //private String className;
-  //private Map<String, String> fields;
+  // private String className;
+  // private Map<String, String> fields;
   public String className;
   public String name;
   public String type;
-  //public Map<String, String> fields;
+  // public Map<String, String> fields;
 
   private void construct_helper(JSONObject jo) {
     JSONArray classes = jo.getJSONArray("classes");
     JSONObject oneclass = classes.getJSONObject(0);
-    //Iterator it = classes.iterator();
-    
+    // Iterator it = classes.iterator();
+
     className = oneclass.getString("name");
     JSONArray ja = oneclass.getJSONArray("fields");
     JSONObject onefield = ja.getJSONObject(0);
     name = onefield.getString("name");
     type = onefield.getString("type");
-    //fields.put(onefield.getString("name"), onefield.getString("type"));
+    // fields.put(onefield.getString("name"), onefield.getString("type"));
   }
+
   public ClassBuilder(String str) {
     JSONObject jo = new JSONObject(str);
-    construct_helper(jo); 
-  }
-  public ClassBuilder(InputStream input) {
-    JSONObject jo = new JSONObject(input); 
     construct_helper(jo);
   }
-}
+
+  public ClassBuilder(InputStream input) {
+    JSONObject jo = new JSONObject(input);
+    construct_helper(jo);
+  }
+
+  public String getSourceCode(/* String className */) {
+    String capName = name.substring(0, 1).toUpperCase() + name.substring(1);
+    return "public class " + className + "{\n private " + type + " " + name + ";\n public int get" + capName
+        + "() {\n return " + name + ";\n }\n  public void set" + capName + "(" + type + " x) { \n this." + name
+        + " = x; }}";
+  }
+
+  public void createAllClasses(String basePath) throws FileNotFoundException {
+    String path = basePath + className + ".java";
+    File file = new File(path);
+    PrintWriter out = new PrintWriter(file);
+    out.println(getSourceCode());
+    out.close();
+  }
+    
+/*
 class Main {
   public static void main(String[] args) {
     String str = "{\"classes\" : [{\"name\" : \"Test\", \"fields\" : [{\"name\" : \"x\", \"type\" : \"int\"}]}]}";
@@ -47,6 +69,7 @@ class Main {
       String v = entry.getValue();
       System.out.println("Key: " + k + ", Value: " + v);
     }
-    */
+    
   }
+*/
 }
--- src/test/java/edu/duke/ece651/classbuilder/CBTest.java
+++ src/test/java/edu/duke/ece651/classbuilder/CBTest.java
@@ -3,20 +3,32 @@ package edu.duke.ece651.classbuilder;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
-import java.util.Map;
+//import java.util.Map;
+import java.io.*;
 
 public class CBTest {
   @Test
   public void test_classbuilder() {
-    String str = "{\"classes\" : [{\"name\" : \"Test\", \"fields\" : [{\"name\" : \"x\", \"type\" : \"int\"}]}]}";
-    ClassBuilder cb = new ClassBuilder(str);
-    assertEquals(cb.className, "Test");
-    assertEquals(cb.name, "x");
-    assertEquals(cb.type, "int");
+    String r = "{\"classes\" : [{\"name\" : \"Test\", \"fields\" : [{\"name\" : \"x\", \"type\" : \"int\"}]}]}";
+                                         
+    //String path = "/home/jaryn/jx95-json-651/json/simple.json";
+    //InputStream inputStream = new FileInputStream(new File(path));   
+   ClassBuilder c = new ClassBuilder(r);
+   System.out.println(c.className);
+   System.out.println(c.name);
+   System.out.println(c.type);
+   System.out.println(c.getSourceCode());
+   c.createAllClasses("/home/jaryn/jx95-json-651");
+    //assertEquals(c.className, "Test");
+    //assertEquals(c.name, "x");
+    //assertEquals(c.type, "int");
     
     
-    
-  }
+    //ClassBuilder cb = new ClassBuilder(str);
   
-
+    
+    
+  
+  
+  }
 }
